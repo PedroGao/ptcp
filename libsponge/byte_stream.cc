0d9a8a5e50ad3c0e9d8ffbc3226d680b7ee22bd9
PedroGao <gaopedro@163.com>
2020-11-03 22:35:13 +0800

    修复 merge node 的一些 bug

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -20,38 +20,32 @@ ByteStream::ByteStream(const size_t capacity) : _capacity(capacity) {}
 
 size_t ByteStream::write(const string &data) {
     size_t len = data.size();
-    if (_capacity - _buffer.size() < len)
-        len = _capacity - _buffer.size();
-    for (size_t i = 0; i < len; i++)
+    size_t will_write = std::min(len, remaining_capacity());
+    for (size_t i = 0; i < will_write; i++) {
         _buffer.push_back(data[i]);
-    _bytes_write += len;
-    return len;
+    }
+    _bytes_write += will_write;
+    return will_write;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    size_t length = len;
-    if (length > _buffer.size())
-        length = _buffer.size();
+    size_t length = std::min(len, _buffer.size());
     return string().assign(_buffer.begin(), _buffer.begin() + length);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    size_t length = len;
-    if (length > _buffer.size())
-        length = _buffer.size();
-    _bytes_read += length;
-    while (length--)
+    size_t length = std::min(len, _buffer.size());
+    for (size_t i = 0; i < length; i++) {
         _buffer.pop_front();
+    }
+    _bytes_read += length;
 }
 
 void ByteStream::end_input() { _end_input = true; }
 
-bool ByteStream::input_ended() const {
-    return _end_input;
-    ;
-}
+bool ByteStream::input_ended() const { return _end_input; }
 
 size_t ByteStream::buffer_size() const { return _buffer.size(); }
 
@@ -63,6 +57,4 @@ size_t ByteStream::bytes_written() const { return _bytes_write; }
 
 size_t ByteStream::bytes_read() const { return _bytes_read; }
 
-size_t ByteStream::remaining_capacity() const {
-    return std::max<std::size_t>(0, _capacity - _bytes_write + _bytes_read);
-}
+size_t ByteStream::remaining_capacity() const { return std::max<size_t>(0, _capacity - _buffer.size()); }
--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -31,12 +31,14 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
     // lower_bound
     // 二分查找一个有序数列，返回第一个大于等于x的数，如果没找到，返回末尾的迭代器位置
     node elem;
-    // 如果当前序号 + 数据长度 <= 头部序号，则表示
-    // 太小
+    // 如果当前序号 + 数据长度 <= 头部序号，则表示太小
+    // 注意：这里的 _head_index 是从 0 开始的，表示 merge 一定会从 0 开始合并
+    // 如果有其它节点则不会合并
     if (index + data.length() <= _head_index) {
         goto JUDGE_EOF;
     } else if (index < _head_index) {
-        // 如果当前的序号小于头部序号
+        // 如果当前的序号小于头部序号，因为 _head_index 之前的 bytes 全部都被合并了
+        // 所以截取掉前端的 offset 字节
         size_t offset = _head_index - index;
         elem.data.assign(data.begin() + offset, data.end());
         elem.begin = index + offset;
@@ -47,6 +49,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         elem.length = data.length();
         elem.data = data;
     }
+    // 新来的节点都是没有被重组的
     _unassembled_byte += elem.length;
     // 合并节点
     do {
@@ -54,7 +57,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         // 向前合并，通过 lower_bound 得到 begin 大于等于 elem begin 的节点
         auto iter = _blocks.lower_bound(elem);
         // 如果不是最后一个，且 elem 能与 iter 合并
-        while (iter != _blocks.end() && (merged_bytes = merge_node(elem, *iter)) > 0) {
+        while (iter != _blocks.end() && (merged_bytes = merge_node(elem, *iter)) >= 0) {
             // 节点重组
             _unassembled_byte -= merged_bytes;
             // iter 被合并，所以删除
@@ -69,7 +72,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         }
         // 合并完后面的之后，遇到 end 或者 二个节点没有交集，则将 iter-- ，得到 iter 前一个节点，看是否能合并
         iter--;
-        while ((merged_bytes = merge_node(elem, *iter)) > 0) {
+        while ((merged_bytes = merge_node(elem, *iter)) >= 0) {
             _unassembled_byte -= merged_bytes;
             _blocks.erase(iter);
             // iter 被合并了，所以要删除它，但是还需要得到 >= elem.begin 的节点
@@ -122,18 +125,24 @@ long StreamReassembler::merge_node(node &node1, const node &node2) {
     // 如果 front.begin + front.length < back.begin
     // 即 front 与 back 之间没有交集
     if (front.begin + front.length < back.begin) {
-        // 压根合并不了，直接返回
-        return 0;
+        // 压根合并不了，直接返回，并且返回 -1
+        return -1;
     } else if (front.begin + front.length >= back.begin + back.length) {
         // 如果 front.begin + front.length >= back.begin + back.length
         // 即 front 完全能够包裹 back，所以无需合并，直接返回 back.length
         node1 = front;
+        // back 的数据已经有了，因此要剪掉重复的部分数据，所以返回 back.length
         return back.length;
     } else {
         // node1 和 node2 有交集
         size_t gap = node2.begin - node1.begin;
-        front.data = front.data.substr(gap) + back.data;
+        size_t merged = front.length - gap;
+        // 截取 front 的前半段
+        front.data = front.data.substr(0, gap) + back.data;
         front.length = gap + back.length;
-        return gap;
+        node1 = front;
+        // 注意，如果 front 和 back 没有交集，则这里的 merged 是 0
+        // 所以 0 不能当做判断的理由
+        return merged;
     }
 }
